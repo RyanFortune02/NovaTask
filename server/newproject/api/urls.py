ea8be21cef0ccfa28ed49e5da7efb53881483e57
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2025-02-18 21:37:41 -0500

    TimeEntry model, serializer, views, and urls
    As requested, a new model and some views to interact with it.
    
    Migrates the database.

--- server/newproject/api/urls.py
+++ server/newproject/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import todos, todo_detail, check_notifications
+from .views import (
+    todos,
+    todo_detail,
+    check_notifications,
+    time_entries,
+    time_entry_detail,
+)
 
 urlpatterns = [
     path("todos/", todos),
     path("todos/<int:pk>/", todo_detail),
     path("todos/notify/", check_notifications),
+    path("times/", time_entries),
+    path("times/<int:fpk>/", time_entry_detail),
 ]
